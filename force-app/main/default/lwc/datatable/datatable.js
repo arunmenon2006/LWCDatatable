a1ba2bd2680e839f99b0b3ccb9b4a9f5d6b4e45c
logicline\amenon <arun.menon@logicline.de>
2019-07-06 13:06:08 +0530

    no message

--- force-app/main/default/lwc/datatable/datatable.js
+++ force-app/main/default/lwc/datatable/datatable.js
@@ -1,6 +1,14 @@
 import { LightningElement, wire } from 'lwc';
 import findRecords from '@salesforce/apex/DatatableCtrl.fetchRecords';
 
+const columns = [
+    {label: 'Account Name', fieldName: 'name', type: 'text'},
+    {label: 'Phone', fieldName: 'phone', type: 'text'},
+    {label: 'Type', fieldName: 'type', type: 'text'},
+    {label: 'website', fieldName: 'website', type: 'text'},
+    {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},
+];
+
 export default class Datatable extends LightningElement {
     @wire(findRecords, {objectName : 'Account', fields : ['Name','Phone', 'Type', 'Website'], noOfRecords : 100, filterCriteria : ''}) records;
 }
